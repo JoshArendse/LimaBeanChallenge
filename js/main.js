1af17fa10ad76acfae70949470b69ed217898667
JoshArendse <42963960+JoshArendse@users.noreply.github.com>
2019-02-21 13:16:14 +0200

    completed webpage
    html and styling components completed, next up, responsiveness

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 window.addEventListener("scroll", function(){
     var y= this.window.pageYOffset;
     this.document.getElementById("banner").style.backgroundPositionY= (y*-0.5)+"px";
+});
+
+AOS.init({
+    duration: 1200,
 })
+
